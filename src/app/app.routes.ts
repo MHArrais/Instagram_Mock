057b757f6eaebf7ef7ae787f00f4db8fa30af495
MHArrais <drake_i@hotmail.com>
2025-02-24 20:14:40 -0300

    feat: finish project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { Component } from '@angular/core';
+import { ExplorePageComponent } from './pages/explore-page/explore-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        component: HomePageComponent,
+        path: ""
+    },
+    {
+        component:ExplorePageComponent,
+        path: "explore/:country"
+    },
+    {
+        path:'**',
+        redirectTo:'',
+    }
+];
